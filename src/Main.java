1663553099643d3fb306b252062b770a8d0bb956
angel <angel.zazueta14@hotmail.com>
2023-08-19 10:33:31 -0700

    Documento CSV

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+    public static final String SEPARADOR =",";
+    public static void main(String[] args) throws IOException {
+
+        String csvFile = "src/car_sales.csv";
+        BufferedReader br = new BufferedReader(new FileReader(csvFile));
+        int numRows = 0;
+        int numCols = 0;
+        String line;
+
+        // Contar el número de filas y columnas
+        while ((line = br.readLine()) != null) {
+            numRows++;
+            String[] row = line.split(SEPARADOR);
+            numCols = Math.max(numCols, row.length);
+        }
+        br.close();
+        br = new BufferedReader(new FileReader(csvFile));
+
+        //Se crea el array con la informacion TOTAL
+        String[][] dataArray = new String[numRows][numCols];
+        int rowIdx = 0;
+
+        while ((line = br.readLine()) != null) {
+            String[] row = line.split(SEPARADOR);
+            System.arraycopy(row, 0, dataArray[rowIdx], 0, row.length);
+            rowIdx++;
+        }
+        br.close();
+
+        int lineaLeer = 0;
+        for (int i = 0; i < numCols; i++) {
+            if(dataArray[0][i].equals("sales")){
+                lineaLeer = i;
+            }
+        }
+
+        //Array con unicamente las sales
+        double[] salesNumber = new double[numRows];
+        for (int i = 1; i < salesNumber.length ; i++) {
+            String tempLine = dataArray[i][lineaLeer].substring(1);
+            salesNumber[i] = Double.parseDouble(tempLine);
+        }
+
+        String userhome = System.getProperty("user.home");
+        String desktopPath = userhome + "\\Desktop\\sumasPrefijasTotal.csv";
+        File desktopFile = new File(desktopPath);
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(desktopFile));
+
+        double suma=0;
+        for (int i = 1; i < salesNumber.length; i++) {
+            suma += salesNumber[i];
+            String formato = String.format("%d,%.2f%n", i, suma);
+            bw.write(formato);
+        } bw.flush();
+
+    }
+}
